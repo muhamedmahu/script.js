a638780071a8ffa9e2cfe01ab38e4e1c0b47712f
muhamedmahu <muhamedmahu@g,ail.com>
2022-02-02 15:58:30 +0000

    script.js

---
+++ script.js/script.js
@@ -0,0 +1,84 @@
+var emptyVaribale;
+var messge = "helo world"
+var firstName, lastName = "Imashov"
+
+var exampleNumber1 = 123;
+var exampleNumber2 = 123.45;
+var exampleNumber3 = Infinity;
+var exampleNumber4 = NaN;
+
+var exampleString1 = "hello world";
+var exampleString2 = 'heloo world';
+var exampleString3 = `hello world`;
+var exampleString4 = "4";
+
+var exampleBoolean1 = true;
+var exampleBoolean2 = false;
+
+var exampleLenull = null;
+var exampleUndefinde1;
+var exampleUndefinde2 = undefined;
+
+var exampleSymbol = Symbol("id");
+
+var exampleObject1 = {};
+var exampleObject2 = Math;
+
+
+//alert("hello world")
+
+//var result = prompt("hello world");
+
+//var result = confirm("hello world");
+
+//document.write("hello wolrld")
+
+var exampleTypeConversion1 = 5 + "5";
+var exampleTypeConversion2 + 5 + 5 + "5"
+var exampleMath1 = 5 + 6;
+var exampleMath2 = 5 - 6;
+var exampleMath3 = 5 / 6;
+var exampleMath4 = 5 * 6;
+
+var exampleMath5 = 5 % 2;
+var exampleMath6 = 6 % 2;
+
+
+var exampleMath7 = 2 ** 4;
+
+
+var exampleMath8 = 4;
+exampleMath8 += 5;
+exampleMath8 -= 3;
+
+
+var exampleMath9 = 0;
+exampleMath9++;
+exampleMath9--;
+++exampleMath9;
+--exampleMath9;
+
+//comparisson
+var exampleComparison1 = 6 > 5;
+var exampleComparison2 = 6 < 5;
+var exampleComparison3 = 6 >= 5;
+var exampleComparison4 = 6 <= 5;
+var exampleComparison5 = 6 === 5;
+var exampleComparison6 = 5 !== 5;
+var exampleComparison7 = "5" == 5;
+var exampleComparison8 = "5" != 5;
+
+
+//var age = prompt("how old are you?");
+//var votingYears = age - 18;
+//if (votingYears === NaN)
+//alert("wrong input. numbers only.");
+//alert("you could've vote for" + votingYears + "years");
+
+
+var userNumber;
+while (userNumber != "3.14") {
+    userNumber = prompt("what iis number");
+}
+
+//help me please
